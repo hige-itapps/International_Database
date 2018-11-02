61c3de3f9618e87908dd665f524ed39e031203d1
skison <samuel.j.kison@wmich.edu>
2018-11-02 11:08:41 -0400

    Added maintenance mode
    maintenance mode can be enabled by copying /maintenance/.htaccess to the root web directory (/client). When this file is present, regular users will be redirected to /maintenance.php with a 503: service unavailable header. Admins can specify IP exceptions so that they can still access the site like usual while others are redirected. To end maintenance mode, just delete the .htaccess file.

---
+++ client/maintenance.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	
+	<!-- Page Head -->
+	<head>
+		<!-- Shared head content -->
+		<?php include 'include/head_content.html'; ?>
+	</head>
+
+	<!-- Page Body -->
+	<body>
+		<!-- Shared Site Banner -->
+		<?php include 'include/site_banner.html'; ?>
+	
+		<div id="MainContent" role="main">
+			<h1>This site is currently down for maintenance. Please come back at a later time.</h1>
+		</div>
+		<!--BODY-->
+	
+	</body>
+	
+</html>
---
+++ maintenance/README.txt
@@ -0,0 +1,3 @@
+During site maintenance, copy the .htaccess file to /client (the root web directory). This will enable maintenance mode, where user traffic will be redirected to /client/maintenance.php.
+
+You can specify IP addresses to be ignored during site maintenance; this way, you can still access the site like normal with your IP while other users still get redirected.
